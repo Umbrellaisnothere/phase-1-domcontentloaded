e68e3b1884358cb08811d3b558ab3216e96b3e9f
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-07-28 14:01:52 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    // Targeting id = "text"
+      const paragraph = document.getElementById("text");
+      paragraph.textContent = "This is really cool!";
+    });
